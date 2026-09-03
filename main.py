21cfc7114a75e6b71d15a0da61f50290286fb021
Dani-nino <danielninoplazas5@gmail.com>
2026-09-03 18:18:02 -0500

    editor de imagenes

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+from PyQt5.QtWidgets import (
+   QApplication, QWidget,
+   QFileDialog, # Diálogo para abrir archivos (y carpetas)
+   QLabel, QPushButton, QListWidget,
+   QHBoxLayout, QVBoxLayout
+)
+
+app = QApplication([])
+win = QWidget()
+win.resize(700, 500)
+win.setWindowTitle('Editor simple')
+lb_image = QLabel("Imagen")
+btn_dir = QPushButton("Carpeta")
+lw_files = QListWidget()
+
+btn_left = QPushButton("Izquierda")
+btn_right = QPushButton("Derecha")
+btn_flip = QPushButton("Reflejo")
+btn_sharp = QPushButton("Nitidez")
+btn_bw = QPushButton("B/N")
+
+row = QHBoxLayout()       
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)
+col1.addWidget(lw_files)
+col2.addWidget(lb_image, 95)
+row_tools = QHBoxLayout()
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_bw)
+col2.addLayout(row_tools)
+
+
+row.addLayout(col1, 20)
+row.addLayout(col2, 80)
+win.setLayout(row)
+
+win.show()
+
+workdir = ''
+def filter(files, extensions):
+    result = []
+    for filename in files:
+         for ext in extensions:
+              if filename.endswith(ext):
+                result.append(filename)
+    return result
+
+
+def chooseWorkdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    
+def showFilenamesList():
+    extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+    chooseWorkdir()
+    filenames = filter(os.listdir(workdir), extensions)
+    lw_files.clear()
+    for filename in filenames:
+       lw_files.addItem(filename)
+       
+btn_dir.clicked.connect(showFilenamesList)
+app.exec()
